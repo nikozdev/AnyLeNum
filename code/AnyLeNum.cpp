df9cf264e1cad178f69af28f3e5528c98d28feaf
nikozdev <nikozdev@nikozdev.net>
2023-08-05 02:33:35 -0700

    !code!test!simple!improve!refactor
    [code/AnyLeNum.cpp] > implemented test selection with std::unordered_map

--- code/AnyLeNum.cpp
+++ code/AnyLeNum.cpp
@@ -6,15 +6,42 @@
 #include "fmt/format.h"
 //-//standard
 #include <cstdlib>
+#include <string_view>
+#include <functional>
+//-//-//template-library
+#include <unordered_map>
 //content
 namespace nAnyLeNum
 {
 }
 #if defined(dAnyLeNumMakeExe)
-int main(int vArgC, char**vArgV)
+auto vCmdTab = std::unordered_map<std::string_view, std::function<bool(void)>>{
+	{"tHello",
+	 []()
+	 {
+		 fmt::println("HelloWorld");
+		 return 1;
+	 }},
+};
+int main(int vArgC, char **vArgV)
 {
-  fmt::println("HelloWorld");
-  return EXIT_SUCCESS;
+	if(vArgC > 1)
+	{
+		auto pCmdFun = vCmdTab.find(vArgV[1]);
+		auto pCmdEnd = vCmdTab.end();
+		if(pCmdFun == pCmdEnd)
+		{
+			return EXIT_FAILURE;
+		}
+		else
+		{
+			return pCmdFun->second() ? EXIT_SUCCESS : EXIT_FAILURE;
+		}
+	}
+	else
+	{
+		return EXIT_FAILURE;
+	}
 }
 #endif//ifd(dAnyLeNumMakeExe)
 //actions
